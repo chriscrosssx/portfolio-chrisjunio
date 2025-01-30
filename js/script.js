57edf6757cef98c15bf9027a3bcefeccfcfa489d
UBSITJunioChristian <20215633@s.ubaguio.edu>
2025-01-30 13:32:43 +0800

    some changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    strings:["","Business Analysis","Project Management","Quality Assurance","Software Testing","Web Designer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
